7aca05b1e5c07de74cd6f36825d943c1117ea44b
Fede Fomin <wffomin@protonmail.com>
2022-02-18 05:09:54 +0100

    AZ sort

--- Sorting/Algorithms.cs
+++ Sorting/Algorithms.cs
@@ -8,7 +8,11 @@ namespace Sorting
 {
     public static class Algorithms
     {
-        public static void BubbleSort(int[] arr)
+        /// <summary>
+        /// Arrange numbers contained in N - 1 elements of an array
+        /// </summary>
+
+        public static void sortNumbers(int[] arr)
         {
             int buffer;
 
@@ -27,6 +31,42 @@ namespace Sorting
 
             for (int k = 0; k < arr.Length; k++)
                 Console.Write(" " + arr[k]);
+        }
+
+        /// <summary>
+        /// Sort a list in an alphabetical order using a two dimensional array.
+        /// </summary>
+
+        public static void sortAZOrder(string[,] arr)
+        {
+            string[] buffer = new string[3];
+
+            for(int i = 0; i < arr.GetLength(0) - 1; i++)
+            {
+                for(int j = 0; j < arr.GetLength(0) - 1; j++)
+                {
+                    if (String.Compare(arr[j, 0], arr[j + 1, 0]) > 0) /// Sorting only the second column.
+                    {
+                        buffer[0] = arr[j, 0];
+                        buffer[1] = arr[j, 1];
+                        buffer[2] = arr[j, 2];
+
+                        arr[j, 0] = arr[j + 1, 0];
+                        arr[j, 1] = arr[j + 1, 1];
+                        arr[j, 2] = arr[j + 1, 2];
+
+                        arr[j + 1, 0] = buffer[0];
+                        arr[j + 1, 1] = buffer[1];
+                        arr[j + 1, 2] = buffer[2];
+
+                    }
+                }
+            }
+
+            for(int k = 0; k < arr.GetLength(0); k++)
+            {
+                Console.WriteLine(arr[k, 0] + " " + arr[k, 1] + " " + arr[k, 2]);
+            }
         }
     }
 }
--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -6,9 +6,23 @@ namespace Sorting
     {
         static void Main(string[] args)
         {
+            // sorting numbers
             int[] nums = new int[5] { 4, 91, 82, 5, 21 };
+            Algorithms.sortNumbers(nums);
 
-            Algorithms.BubbleSort(nums);
+            Console.Write("\n\n");
+
+            // sorting a list in an alphabetical order
+            string[,] people = new string[5, 3]
+            {
+                { "Mario", "Rossi", "24" },
+                { "Valerio", "Nero", "32" },
+                { "Alessandro", "DeNegri", "21" },
+                { "Francesco", "DellaTorre", "12" },
+                { "Marco", "Francia", "21" }
+            };
+
+            Algorithms.sortAZOrder(people);
         }
     }
 }
